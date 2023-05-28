ae816994794dd9da47382e6d27671ba9abaea4cd
Anastasia_Zeland <Anastasia.Zeland@urfu.me>
2023-05-28 16:25:52 +0500

    сделала адаптив

---
+++ src/app/components/places-settings/places-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-places-settings',
+  templateUrl: './places-settings.component.html',
+  styleUrls: ['./places-settings.component.css']
+})
+export class PlacesSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
